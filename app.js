bacbf299c74134c571361d318857b84503733e58
tm0605 <takuyamiyamo65@gmail.com>
2022-11-01 11:07:50 +1000

    Photo upload and editing feature

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const fileUpload = require('express-fileupload');
+const sharp = require('sharp');
 
+const fileExists = require('./middleware/fileExists');
+const { type } = require('os');
 // var indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
 // var uploadRouter = require('./routes/upload');
@@ -34,9 +37,45 @@ app.get('/resize', function(req, res) {
 // app.use('/upload', uploadRouter);
 app.post('/upload', 
   fileUpload({ createParentPath: true }),
+  fileExists,
   (req, res) => {
-    const files = req.files
-    console.log(files)
+    // let start = Date.now();
+    var files = req.files.photos
+    if (!(files instanceof Array)) { // If single photo add it in an array
+      files = [files];
+    }
+
+    Object.keys(files).forEach(async key => { // For each photo object in files array
+      // console.log(files[key].data)
+      // photos[key] = files[key]
+      const name = files[key].name
+      const info = await sharp(files[key].data)
+        .greyscale()
+        .jpeg()
+        .toFile(`images/test${key}.jpeg`);
+      const scaleHalf = await sharp(files[key].data)
+        .metadata()
+        .then(({ width }) => sharp(files[key].data)
+          .resize(Math.round(width * 0.5))
+          // .toBuffer()
+          .toFile(`images/testscalehalf${key}.jpeg`)
+          /* .then(function(outputBuffer) {
+            // outputBuffer contains image data
+          }) */);
+      const scale = await sharp(files[key].data)
+        .resize({ width: 100 })
+        .toBuffer()
+        .then(data => {
+          console.log(data)
+          // 100 px wide, auto scale hight
+        })
+      // console.log(info)
+      // console.log(scaleHalf)
+      // console.log(scale)
+    })
+    // let timing = Date.now() - start;
+
+    // console.log(photos)
 
     return res.json({ status: 'logged', message: files })
   }
---
+++ public/javascripts/imageUpload.js
@@ -0,0 +1,36 @@
+const form = document.getElementById("uploadForm");
+
+const sendFiles = async () => {
+    let start = Date.now();
+    const files = document.getElementById("inputFiles").files;
+
+    const formData = new FormData();
+
+    Object.keys(files).forEach(key => {
+        formData.append('photos', files.item(key))
+    })
+
+    const response = await fetch('/upload', {
+        method: "POST",
+        body: formData
+    })
+
+    const json = await response.json()
+    let timing = Date.now() - start;
+
+    const h2 = document.querySelector('h2')
+    h2.textContent = `Status: ${json?.status}`
+
+    const h3 = document.querySelector('h3')
+    h3.textContent = json?.message
+
+    const p = document.querySelector('p')
+    p.textContent = `${timing} ms`
+
+    console.log(json)
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    sendFiles()
+})

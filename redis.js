37b1dbd7dff583060a0d237f41cc806b3192cc31
tm0605 <takuyamiyamo65@gmail.com>
2022-11-03 01:16:02 +1000

    Redis Test

---
+++ redis.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const { route } = require(".");
+var router = express.Router();
+const redis = require("redis");
+
+const redisClient = redis.createClient({
+    host: "cab432a2image-001.km2jzi.0001.apse2.cache.amazonaws.com",
+    port: "6379"
+});
+// const redisClient = redis.createClient({
+//     url: 'redis://cab432a2image-001.km2jzi.0001.apse2.cache.amazonaws.com:6379'
+// });
+
+redisClient.connect();
+
+redisClient.on('connect', function () {
+    console.log('redis connected');
+})
+
+redisClient.on('error', (err) => {
+    console.log('Error '+ err);
+})
+
+redisClient.setEx('abcd', 3600, 'test'); // Redisに送る
+
+redisClient.get('abcd').then((result) => { //Redisから引っ張ってくる
+    if (result) {
+        console.log(result);
+    }
+    else {
+        console.log('no result');
+    }
+})

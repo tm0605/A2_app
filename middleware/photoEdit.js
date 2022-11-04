2852c89c7dc9bf6bc214829d18d51da0c0945716
tm0605 <takuyamiyamo65@gmail.com>
2022-11-04 18:49:20 +1000

    Changed processing logic

--- middleware/photoEdit.js
+++ middleware/photoEdit.js
@@ -1,190 +1,151 @@
 const sharp = require("sharp");
 
 const photoEdit = (req, res, next) => {
-  let start = Date.now();
   var files = req.files.photos;
   // console.log(files)
-  if (!(files instanceof Array)) {
+
     // If single photo add it in an array
+  if (!(files instanceof Array)) {
     files = [files];
   }
-  // console.log(files)
 
-  var photos = [];
-  var px = 100;
-  var ratio = 0.1;
+  const processing = req.query.processing;
+  const photos = [];
 
   greyscale(files).then(() => {
-    let time = Date.now() - start;
-    console.log(`greyscale processing: ${time}ms`);
-    req.gsPhotos = photos;
-    photos = [];
-    start = Date.now();
-    // resizeByPixel(files, px)
-    // .then(() => {
-    //     time = Date.now() - start;
-    //     console.log(`resize by pixel of ${px} processing: ${time}ms`);
-    //     req.resizeByPixel = photos;
-    //     photos = [];
-    //     start = Date.now();
-    // resizeByRatio(files, ratio)
-    // .then(() => {
-    //     time = Date.now() - start;
-    //     console.log(`resize by ratio of ${ratio} processing: ${time}ms`)
-    //     req.resizeByRatio = photos;
-    //     photos = [];
-    //     start = Date.now();
+    
     resizeHalf(files).then(() => {
-      time = Date.now() - start;
-      console.log(`resize half processing: ${time}ms`);
-      req.resizeHalf = photos;
-      photos = [];
-      normalize(files).then(() => {
-        time = Date.now() - start;
-        console.log(`sharpen processing: ${time}ms`);
-        req.sharpen = photos;
-        photos = [];
+
+      sharpen(files).then(() => {
+        
         blur(files).then(() => {
-          time = Date.now() - start;
-          console.log(`blur processing: ${time}ms`);
-          req.blur = photos;
-          photos = [];
+
+          req.editedPhotos = photos;
           next();
         });
       });
     });
-    // })
-
-    // })
   });
 
   function greyscale(photoset) {
     return new Promise((resolve, reject) => {
-      console.log("starting greyscale processing");
-      // console.log(photoset)
-      for (let n = 0; n < photoset.length; ++n) {
-        const image = sharp(photoset[n].data);
-        image
+      if (processing == 'all' || processing == 'greyscale') {
+
+        let start = Date.now();
+        console.log("starting greyscale processing");
+        // console.log(photoset)
+        for (let n = 0; n < photoset.length; ++n) {
+          const image = sharp(photoset[n].data);
+          image
           .greyscale()
           .toBuffer()
           .then((data) => {
             photos.push(data);
             if (n == photoset.length - 1) {
+              let time = Date.now() - start;
+              console.log(`greyscale processing: ${time}ms`);
               resolve();
             }
           })
           .catch((error) => {
             console.log(error);
           });
+        }
       }
-    });
-  }
-
-  function resizeByPixel(photoset, px) {
-    return new Promise((resolve, reject) => {
-      console.log("starting resize process");
-      for (let n = 0; n < photoset.length; ++n) {
-        const image = sharp(photoset[n].data);
-        image
-          .resize({ width: px })
-          .toBuffer()
-          .then((data) => {
-            photos.push(data);
-            if (n == photoset.length - 1) {
-              resolve();
-            }
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    });
-  }
-
-  function resizeByRatio(photoset, ratio) {
-    return new Promise((resolve, reject) => {
-      console.log(`starting half resize process`);
-      for (let n = 0; n < photoset.length; ++n) {
-        const image = sharp(photoset[n].data);
-        image.metadata().then(({ width }) => {
-          image
-            .resize(Math.round(width * ratio))
-            .toBuffer()
-            .then((data) => {
-              photos.push(data);
-              if (n == photoset.length - 1) {
-                resolve();
-              }
-            })
-            .catch((error) => {
-              console.log(error);
-            });
-        });
+      else {
+        resolve();
       }
     });
   }
 
   function resizeHalf(photoset) {
     return new Promise((resolve, reject) => {
-      console.log(`starting half resize process`);
-      for (let n = 0; n < photoset.length; ++n) {
-        const image = sharp(photoset[n].data);
-        image.metadata().then(({ width }) => {
-          image
+      if (processing == 'all' || processing == 'resize') {
+
+        let start = Date.now();
+        console.log(`starting half resize process`);
+        for (let n = 0; n < photoset.length; ++n) {
+          const image = sharp(photoset[n].data);
+          image.metadata().then(({ width }) => {
+            image
             .resize(Math.round(width * 0.5))
             .toBuffer()
             .then((data) => {
               photos.push(data);
               if (n == photoset.length - 1) {
+                let time = Date.now() - start;
+                console.log(`resize half processing: ${time}ms`);
                 resolve();
               }
             })
             .catch((error) => {
               console.log(error);
             });
-        });
+          });
+        }
+      }
+      else {
+        resolve();
       }
     });
   }
 
-  function normalize(photoset) {
+  function sharpen(photoset) {
     return new Promise((resolve, reject) => {
-      console.log("starting sharpen process");
-      for (let n = 0; n < photoset.length; ++n) {
-        const image = sharp(photoset[n].data);
-        image
+      if (processing == 'all' || processing == 'sharpen') {
+
+        let start = Date.now();
+        console.log("starting sharpen process");
+        for (let n = 0; n < photoset.length; ++n) {
+          const image = sharp(photoset[n].data);
+          image
           .sharpen({ sigma: 20 })
           .toBuffer()
           .then((data) => {
             photos.push(data);
             if (n == photoset.length - 1) {
+              let time = Date.now() - start;
+              console.log(`sharpen processing: ${time}ms`);
               resolve();
             }
           })
           .catch((error) => {
             console.log(error);
           });
+        }
+      }
+      else {
+        resolve();
       }
     });
   }
 
   function blur(photoset) {
     return new Promise((resolve, reject) => {
-      console.log("starting blur process");
-      for (let n = 0; n < photoset.length; ++n) {
-        const image = sharp(photoset[n].data);
-        image
+      if (processing == 'all' || processing == 'blur') {
+
+        let start = Date.now();
+        console.log("starting blur process");
+        for (let n = 0; n < photoset.length; ++n) {
+          const image = sharp(photoset[n].data);
+          image
           .blur(20)
           .toBuffer()
           .then((data) => {
             photos.push(data);
             if (n == photoset.length - 1) {
+              let time = Date.now() - start;
+              console.log(`blur processing: ${time}ms`);
               resolve();
             }
           })
           .catch((error) => {
             console.log(error);
           });
+        }
+      }
+      else {
+        resolve();
       }
     });
   }
